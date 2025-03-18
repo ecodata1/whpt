dbe0df56a41fbf6115563154fda7a45eb5550271
Foster <Tim.Foster@SEPA.org.uk>
2025-03-18 14:51:21 +0000

    lint: remove commented coded

--- tests/testthat/test-whpt-predict.R
+++ tests/testthat/test-whpt-predict.R
@@ -2,14 +2,4 @@ test_that("predict works", {
   data <- demo_data
   predictions <- whpt_predict(demo_data)
 
-
-  # data <- utils::read.csv(system.file("extdat",
-  #                                           "predictors.csv",
-  #                                           package = "whpt"
-  # ))
-  #
-  # data$sample_id <-   data$location_id
-  # data$date_taken <- "2022-02-04"
-  # data$question <-
-  # whpt_predict(predictors)
 })
